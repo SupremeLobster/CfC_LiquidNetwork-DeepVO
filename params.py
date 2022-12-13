f16cd1cff3e4a7c9d0116d7ef1d3177befb24f25
SupremeLobster <u1956231@campus.udg.edu>
2022-12-13 22:51:51 +0100

    - Finishing touches

--- params.py
+++ params.py
@@ -2,28 +2,36 @@ import os
 
 class Parameters():
     def __init__(self):
-        self.n_processors = 8
+        self.n_processors = 4
         # Path\
         self.data_dir =  'C:\\Users\\vilad\\OneDrive - campus.udg.edu\\Documents [UdG-GEINF]\\2022-2023\\Tècniques Avançades d\'Intel·ligència Artifcial [P.Inf]\\Projecte\\dataset\\dataset\\'
         self.image_dir = self.data_dir + 'sequences\\'
         self.pose_dir = self.data_dir + 'poses\\'
 
+        
         self.train_video = ['00', '01', '02', '05', '08', '09']
         self.valid_video = ['04', '06', '07', '10']
+#         self.train_video = ['00', '02', '08', '09']  # As used in original paper
+#         self.valid_video = ['03', '04', '05', '06', '07', '10']  # As used in original paper
         self.partition = None  # partition videos in 'train_video' to train / valid dataset  #0.8
 
 
         # Data Preprocessing
         self.resize_mode = 'rescale'  # choice: 'crop' 'rescale' None
-        self.img_w = 608   # original size is about 1226
-        self.img_h = 184   # original size is about 370
+        self.img_w = 304   # original size is about 1226
+#         self.img_w = 456
+#         self.img_w = 608   # original size is about 1226
+        self.img_h = 92   # original size is about 370
+#         self.img_h = 138
+#         self.img_h = 184   # original size is about 370
         self.img_means = -0.1389521052731338
         self.img_means_np = 92.0672126539245
         self.img_stds = 0.2911982605213023
         self.img_stds_np = 74.25555314703192
         self.minus_point_5 = True
 
-        self.seq_len = (5, 7)
+#         self.seq_len = (5, 7)
+        self.seq_len = (10, 14)
         self.sample_times = 1
 
         # Data info path
@@ -42,9 +50,10 @@ class Parameters():
         self.epochs = 250
         self.batch_size = 8
         self.pin_mem = True
-        self.optim = {'opt': 'Adagrad', 'lr': 0.0005}
+        self.optim = {'opt': 'AdamW', 'base_lr': 0.002, 'decay_lr': 0.97, 'weight_decay': 0.0001}
                     # Choice:
-                    # {'opt': 'Adagrad', 'lr': 0.001}
+                    # {'opt': 'AdamW', 'base_lr': 0.002, 'decay_lr': 0.97, 'weight_decay': 0.0001}
+                    # {'opt': 'Adagrad', 'lr': 0.0005}
                     # {'opt': 'Adam'}
                     # {'opt': 'Cosine', 'T': 100 , 'lr': 0.001}
 
@@ -55,13 +64,13 @@ class Parameters():
                                 # './pretrained/flownets_bn_EPE2.459.pth.tar'  
                                 # './pretrained/flownets_EPE1.951.pth.tar'
         self.resume = False  # resume training
-        self.resume_t_or_v = '.train'
-        self.load_model_path = 'models/t{}_v{}_im{}x{}_s{}x{}_b{}_rnn{}_{}.model{}'.format(''.join(self.train_video), ''.join(self.valid_video), self.img_h, self.img_w, self.seq_len[0], self.seq_len[1], self.batch_size, self.rnn_hidden_size, '_'.join([k+str(v) for k, v in self.optim.items()]), self.resume_t_or_v)
-        self.load_optimizer_path = 'models/t{}_v{}_im{}x{}_s{}x{}_b{}_rnn{}_{}.optimizer{}'.format(''.join(self.train_video), ''.join(self.valid_video), self.img_h, self.img_w, self.seq_len[0], self.seq_len[1], self.batch_size, self.rnn_hidden_size, '_'.join([k+str(v) for k, v in self.optim.items()]), self.resume_t_or_v)
+        self.resume_t_or_v = '.valid'
+        self.load_model_path = 'models/t{}_v{}_im{}x{}_s{}x{}_b{}_lstm{}_{}.model{}'.format(''.join(self.train_video), ''.join(self.valid_video), self.img_h, self.img_w, self.seq_len[0], self.seq_len[1], self.batch_size, self.rnn_hidden_size, '_'.join([k+str(v) for k, v in self.optim.items()]), self.resume_t_or_v)
+        self.load_optimizer_path = 'models/t{}_v{}_im{}x{}_s{}x{}_b{}_lstm{}_{}.optimizer{}'.format(''.join(self.train_video), ''.join(self.valid_video), self.img_h, self.img_w, self.seq_len[0], self.seq_len[1], self.batch_size, self.rnn_hidden_size, '_'.join([k+str(v) for k, v in self.optim.items()]), self.resume_t_or_v)
 
-        self.record_path = 'records/t{}_v{}_im{}x{}_s{}x{}_b{}_rnn{}_{}.txt'.format(''.join(self.train_video), ''.join(self.valid_video), self.img_h, self.img_w, self.seq_len[0], self.seq_len[1], self.batch_size, self.rnn_hidden_size, '_'.join([k+str(v) for k, v in self.optim.items()]))
-        self.save_model_path = 'models/t{}_v{}_im{}x{}_s{}x{}_b{}_rnn{}_{}.model'.format(''.join(self.train_video), ''.join(self.valid_video), self.img_h, self.img_w, self.seq_len[0], self.seq_len[1], self.batch_size, self.rnn_hidden_size, '_'.join([k+str(v) for k, v in self.optim.items()]))
-        self.save_optimzer_path = 'models/t{}_v{}_im{}x{}_s{}x{}_b{}_rnn{}_{}.optimizer'.format(''.join(self.train_video), ''.join(self.valid_video), self.img_h, self.img_w, self.seq_len[0], self.seq_len[1], self.batch_size, self.rnn_hidden_size, '_'.join([k+str(v) for k, v in self.optim.items()]))
+        self.record_path = 'records/t{}_v{}_im{}x{}_s{}x{}_b{}_lstm{}_{}.txt'.format(''.join(self.train_video), ''.join(self.valid_video), self.img_h, self.img_w, self.seq_len[0], self.seq_len[1], self.batch_size, self.rnn_hidden_size, '_'.join([k+str(v) for k, v in self.optim.items()]))
+        self.save_model_path = 'models/t{}_v{}_im{}x{}_s{}x{}_b{}_lstm{}_{}.model'.format(''.join(self.train_video), ''.join(self.valid_video), self.img_h, self.img_w, self.seq_len[0], self.seq_len[1], self.batch_size, self.rnn_hidden_size, '_'.join([k+str(v) for k, v in self.optim.items()]))
+        self.save_optimzer_path = 'models/t{}_v{}_im{}x{}_s{}x{}_b{}_lstm{}_{}.optimizer'.format(''.join(self.train_video), ''.join(self.valid_video), self.img_h, self.img_w, self.seq_len[0], self.seq_len[1], self.batch_size, self.rnn_hidden_size, '_'.join([k+str(v) for k, v in self.optim.items()]))
 
 
         if not os.path.isdir(os.path.dirname(self.record_path)):
@@ -74,4 +83,17 @@ class Parameters():
             os.makedirs(os.path.dirname(self.train_data_info_path))
 
 par = Parameters()
+
+CFC = {
+    "clipnorm": 0,
+    "hidden_size": par.rnn_hidden_size,
+    "backbone_activation": "silu",
+    "backbone_units": 64,
+    "backbone_layers": 1,
+    "backbone_dr": 0.5,
+    "tau": 10,
+    "batch_size": par.batch_size,
+    "init": 0.84,
+    "use_mixed": True,
+}
 
